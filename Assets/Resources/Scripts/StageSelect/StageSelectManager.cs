337fdd436309d6214545f60d5bbe545a0db0dc03
ilsang <pchm20@gmail.com>
2024-08-03 01:40:40 +0900

    2024-08-03 StageSelect 씬 기반 작업

---
+++ Assets/Resources/Scripts/StageSelect/StageSelectManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelectManager : MonoBehaviour
+{
+    public static StageSelectManager instance;
+
+    private void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            destroy(this);
+        }
+    }
+}
