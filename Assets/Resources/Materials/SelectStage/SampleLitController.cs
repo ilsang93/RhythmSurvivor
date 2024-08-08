b2c26e3c0bd46bc91e0b48e3f99e40d99bfecfca
ilsang <pchm20@gmail.com>
2024-08-09 03:56:26 +0900

    2024-08-09 쉐이더 적용 연습

---
+++ Assets/Resources/Materials/SelectStage/SampleLitController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleLitController : MonoBehaviour
+{
+    [SerializeField] private Material _material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update
+        _material.SetFloat("_SplitValue", Mathf.Sin(Time.time));
+    }
+}
